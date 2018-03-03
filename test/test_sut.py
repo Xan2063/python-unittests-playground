a7f033794dad9fd6e3a4c2cad2aa3bb0655c33b1
Xan2063 <alex.max.stark@gmail.com>
2018-03-03 10:00:48 +0100

    cleanup

--- test/test_sut.py
+++ test/test_sut.py
@@ -1,24 +1,19 @@
 import src
 
 import mock
-import unittest
+import pytest
+
+    
+@mock.patch('src.sut.os')
+def test_filesystem(mock_os):
+    src.sut.dofilesystem('any path')
+    mock_os.remove.assert_called_with('any path')
 
 
-class RmTestCase(unittest.TestCase):
-    @mock.patch('src.sut.pd')
-    @mock.patch('src.sut.os')
-    @mock.patch('src.sut.logging')
-    def test_rm(self, mock_log, mock_os,mock_pandas):
-        mock_pandas.DataFrame.return_value.__getitem__.return_value.mean.return_value=5
-        a = src.sut.remove("any path")
-        assert a== 5 
-        # mock_mean=mock.Mock()
-        # mock_mean = mock_pndas.__getitem__.return_value
-        # self.assertEqual(a, mock_mean)
-        # test that rm called os.remove wimth the right parameters
-        print("lalala")
-        print(a)
-        print("alalal")
-        # assert(mock_log.warn.called)
-        mock_log.warn.assert_called_with("test")
-        # mock_os.remove.assert_called_with("any path")
+@mock.patch('src.sut.logging')
+@mock.patch('src.sut.pd')
+def test_rm(mock_pandas, mock_log):
+    mock_pandas.DataFrame.return_value.__getitem__.return_value.mean.return_value=5
+    a = src.sut.dopandas()
+    mock_log.warn.assert_called_with('test')
+    assert a== 5 
